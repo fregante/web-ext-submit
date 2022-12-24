9e3213cd43f05abbf844685e30673b763199a46f
Dima Gerasimov <karlicoss@gmail.com>
2022-12-24 17:57:55 +0000

    pass through all exit codes except 1 in case of unrelated errors

--- extender.sh
+++ extender.sh
@@ -12,6 +12,11 @@ ok="Your add-on has been submitted for review."
 # chop off everything after "$ok" (it contains distracting stacktrace)
 web-ext "$@" | sed "/$ok/q" | tee "$tmp"
 error=${PIPESTATUS[0]}
+# pass other errors intact
+if [ $error != 1 ] ; then
+	exit $error
+fi
+# defensive if add-on was submitted
 if ! grep -q "$ok" "$tmp" && [ $error = 1 ] ; then
 	exit $error
 fi
