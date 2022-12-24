63178a81e217df2eca6c721ff0842e2d8f16ae76
Dima Gerasimov <karlicoss@gmail.com>
2022-12-24 17:51:57 +0000

    make sed command simpler/more portable so it works on mac os as well
    should address https://github.com/fregante/web-ext-submit/issues/1

--- extender.sh
+++ extender.sh
@@ -9,7 +9,8 @@ cd - > /dev/null || exit
 set -- "sign" "${@:1}" # https://stackoverflow.com/a/4827707/288906
 tmp="$(mktemp)"
 ok="Your add-on has been submitted for review."
-web-ext "$@" | sed -n "s/\($ok\).*$/\0/;1,/$ok/p" | tee "$tmp"
+# chop off everything after "$ok" (it contains distracting stacktrace)
+web-ext "$@" | sed "/$ok/q" | tee "$tmp"
 error=${PIPESTATUS[0]}
 if ! grep -q "$ok" "$tmp" && [ $error = 1 ] ; then
 	exit $error
